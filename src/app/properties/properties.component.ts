4c0cd32e73633d9a73d528c2fe1647681fd1c976
Thomas Johander <tom.johander@dealerteamwork.com>
2019-05-18 13:36:17 -0500

    Data binding working in Template

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertiesComponent implements OnInit {
 
+  property_holding = {
+    "name": "Minneapolis Charter School",
+    "address": {
+      "street_address": "123 Johnson Blvd",
+      "street_address_2": "",
+      "city": "Minneapolis",
+      "state": "Minnesota",
+      "zipcode": "55408"
+    },
+    "market_value": 500000,
+    "tenant": "STEM Charters, LLC"
+  };
+
   constructor() { }
 
   ngOnInit() {
