6d04a0accfd6e7c39ea2eb5fb4e3c1ca9f3fcbfa
Thomas Johander <tom.johander@dealerteamwork.com>
2019-05-18 10:01:15 -0500

    Added Basic routing and Overview component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UsersComponent} from "./users/users.component";
+import {ClientsComponent} from "./clients/clients.component";
+import {PropertiesComponent} from "./properties/properties.component";
+import {OverviewComponent} from "./overview/overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'clients', component: ClientsComponent},
+  { path: 'properties', component: PropertiesComponent},
+  { path: 'overview', component: OverviewComponent },
+
+  { path: '', component: OverviewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { ClientsComponent } from './clients/clients.component';
 import { PropertiesComponent } from './properties/properties.component';
+import { OverviewComponent } from './overview/overview.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     ClientsComponent,
-    PropertiesComponent
+    PropertiesComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
